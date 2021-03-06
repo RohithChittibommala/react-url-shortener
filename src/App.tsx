b1272dd56542f30a8b38e0360f2d3d220c349cb4
Rohith ch <myselfrohith@outlook.com>
2021-03-06 11:22:45 +0530

    intial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import React, { useEffect, useRef, useState } from "react";
-import logo from "./logo.svg";
+import { useRef } from "react";
 import "./App.css";
 
 function App() {
   const BASE_URL = `https://cors.bridged.cc/https://cutt.ly/api/api.php`;
   const ref = useRef<HTMLInputElement>(null);
-
   const handleUrlShorten = () => {
     fetch(
-      `${BASE_URL}?key=0ff7cfe93b777b337f48149939bf8d4baecdc&short=${ref.current?.value}`
+      `${BASE_URL}?key=${process.env.REACT_APP_API_KEY}&short=${ref.current?.value}`
     )
       .then((res) => res.json())
-      .then((res) => console.log(res));
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
   };
 
   return (
