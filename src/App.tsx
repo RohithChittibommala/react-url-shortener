4b85376d9be74be9aabef2d0e578576b491167a9
Rohith ch <myselfrohith@outlook.com>
2021-03-06 11:03:38 +0530

    intial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const BASE_URL = `https://cors.bridged.cc/https://cutt.ly/api/api.php`;
+  const ref = useRef<HTMLInputElement>(null);
+
+  const handleUrlShorten = () => {
+    fetch(
+      `${BASE_URL}?key=0ff7cfe93b777b337f48149939bf8d4baecdc&short=${ref.current?.value}`
+    )
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" ref={ref} />
+      <div className="shorten" onClick={handleUrlShorten}>
+        shorten
+      </div>
     </div>
   );
 }
